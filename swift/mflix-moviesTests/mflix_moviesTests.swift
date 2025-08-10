5a98370fca937741b4ea697fcf9a137b5890c748
Aaron LaBeau <alabeau@gmail.com>
2025-08-10 12:42:57 -0500

    feat: initial check-in for Swift

---
+++ swift/mflix-moviesTests/mflix_moviesTests.swift
@@ -0,0 +1,16 @@
+//
+//  mflix_moviesTests.swift
+//  mflix-moviesTests
+//
+//  Created by Aaron LaBeau on 8/8/25.
+//
+
+import Testing
+
+struct mflix_moviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
